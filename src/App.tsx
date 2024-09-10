5421724634332d8c9a21f1b6578e0821e02be3b2
Pratyush <pratyushjain1916@gmail.com>
2024-09-10 13:40:13 +0530

    Tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import BentoBox from './components/BentoBox/BentoBox';
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <BentoBox />
     </div>
   );
 }
---
+++ src/components/BentoBox/BentoBox.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const BentoBox = () => {
+  return (
+    <div className='main-container'>
+        <div className='left-part'>
+            <div className='upper-part'>
+
+            </div>
+            <div className='lower-part'>
+
+            </div>
+        </div>
+        <div className='right-part'>
+
+        </div>
+    </div>
+  )
+}
+
+export default BentoBox
